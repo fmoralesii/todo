9b17afdc13c6c1ed9dcbf4bc8305b533e390b07f
Frank L Morales II <frankmorales81@outlook.com>
2019-05-31 13:58:52 -0700

    Skeleton methods added

--- todo.py
+++ todo.py
@@ -2,8 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def todo():
-    return 'Welcome to my todo app!'
+@app.route('/todos')
+def showTodos():
+    return 'This page shows all TODOs'
+
+@app.route('/todo/add')
+def addTodo():
+    return 'This page lets me add a TODO'
+
+@app.route('/todo/<int:todo_id>/edit')
+def editTodo(todo_id):
+    return 'This page lets me edit a TODO'
+
+@app.route('/todo/<int:todo_id>/delete')
+def deleteTodo(todo_id):
+    return 'This page lets me delete a TODO'
+
+@app.route('/todo/completed')
+def showCompleted():
+    return 'This page shows all completed TODOs'
 
 if __name__ == "__main__":
     app.debug = True
