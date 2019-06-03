ecbb0d6147a5af95b5fdb7f29de6c524226cb641
Frank L Morales II <frankmorales81@outlook.com>
2019-06-03 12:52:50 -0700

    Added initial HTML template support

--- todo.py
+++ todo.py
@@ -1,26 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# Show only TODOs not completed
 @app.route('/')
 @app.route('/todos')
 def showTodos():
-    return 'This page shows all TODOs'
+    #return 'This page shows all TODOs'
+    dummyData = [
+        {'name' : 'Pickup kids', 'completed' : False},
+        {'name' : 'Walk dog', 'completed' : False}
+    ]
+    return render_template('todo.html', todos = dummyData)
 
 @app.route('/todo/add')
 def addTodo():
-    return 'This page lets me add a TODO'
+    return render_template('addTodo.html')
 
 @app.route('/todo/<int:todo_id>/edit')
 def editTodo(todo_id):
-    return 'This page lets me edit a TODO'
+    return render_template('editTodo.html', todo_id = todo_id)
 
 @app.route('/todo/<int:todo_id>/delete')
 def deleteTodo(todo_id):
-    return 'This page lets me delete a TODO'
+    return render_template('deleteTodo.html', todo_id = todo_id)
 
 @app.route('/todo/completed')
 def showCompleted():
-    return 'This page shows all completed TODOs'
+    return render_template('completed.html', completed_todos = None)
 
 if __name__ == "__main__":
     app.debug = True
